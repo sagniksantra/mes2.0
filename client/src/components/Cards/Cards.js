5a0b9485f7050209a168ddc8274686a8c16086e2
Sagnik <santrasagnik10@gmail.com>
2023-02-08 00:54:22 +0530

    added Cards

---
+++ client/src/components/Cards/Cards.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out our EPIC Events!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+              label='Infotainment'
+              path='/innovationMela'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event abc'
+              label='Marketing'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 123'
+              label='Consulting'
+              path='/register'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 123'
+              label='Consulting'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 456'
+              label='Consulting'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+              label='Genre/topic'
+              path='/register'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 123'
+              label='Consulting'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 456'
+              label='Consulting'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+              label='Genre/topic'
+              path='/register'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 123'
+              label='Consulting'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Event 456'
+              label='Consulting'
+              path='/register'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+              text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+              label='Genre/topic'
+              path='/register'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
